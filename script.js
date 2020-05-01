f8a9c1f037966069f73113925dc44fd8bbffaedb
Danny Befumo <DPBefumo@gmail.com>
2020-05-01 11:58:25 -0400

    Initial JS set up

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 console.log('Add validation!');
+
+const form = document.querySelector("#parking-form")
+console.log(form)
+
+const name = document.querySelector("#name")
+
+form.addEventListener("submit", function(event){
+    event.preventDefault()
+    // add validation functions here 
+    validateName ()
+})
+
+
+// function validateName() {
+//      if (name == "") {
+//          alert ("Please provide your name!")
+//          document.getElementById("name").focus ()
+//          return false
+//      }
+//     return (true)
+}
+// name.addEventListener("input", function(event){
+//     if 
+// })
