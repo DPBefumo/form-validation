7554ab31edcc6d2980899d285125f728d856837c
Danny Befumo <DPBefumo@gmail.com>
2020-05-01 15:19:25 -0400

    Step 1 complete

--- script.js
+++ script.js
@@ -1,57 +1,51 @@
 console.log('Add validation!');
 
-const form = document.querySelector("#parking-form")
-console.log(form)
+let form = document.querySelector("#parking-form")
+let inputFields = document.querySelectorAll("input")
 
 form.addEventListener("submit", function(event){
     event.preventDefault()
+    for (let inputField of inputFields) {
+        let inputValue = inputField.value
+        let parentEl = inputField.parentElement
+        if (inputValue.length > 0) {
+            parentEl.classList.remove("input-invalid")
+            parentEl.classList.add("input-valid")
+        } else {
+            parentEl.classList.remove("input-valid")
+            parentEl.classList.add("input-invalid")
+        }
+    }    
     // add validation functions here 
-    validateName ()
-    valiadteCar ()
-    validateDate ()
-    validateDays ()
-    validateCC ()
-    validateCVV ()
-    validateExpiration ()
+    // validateName ()
+    // valiadteCar ()
+    // validateDate ()
+    // validateDays ()
+    // validateCC ()
+    // validateCVV ()
+    // validateExpiration ()
 })
 
-function validateName() {
-    const name = document.querySelector("#name")
-    const nameValue = name.value
-    const parentEl = name.parentElement
-    
-    if (nameValue) {
-        console.log("name is valid")
-        parentEl.classList.remove("input-invalid")
-        parentEl.classList.add("input-valid")
-    } else {
-        console.log("name is not valid")
-        parentEl.classList.remove("input-valid")
-        parentEl.classList.add("input-invalid")
-    }
+// function valiadteCar () {
 
-}
+// }
 
-function valiadteCar () {
+// function validateDate () {
 
-}
+// } 
 
-function validateDate () {
+// function validateDays () {
 
-} 
+// }
 
-function validateDays () {
+// function validateCC () {
 
-}
+// }
 
-function validateCC () {
+// function validateCVV () {
 
-}
+// }
 
-function validateCVV () {
-    
-}
+// function validateExpiration () {
 
-function validateExpiration () {
-
-}
+// }
