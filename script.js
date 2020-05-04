da3f85e9b6b097cdc7c3a4e6349732aeb52e1c2e
Danny Befumo <DPBefumo@gmail.com>
2020-05-03 21:33:30 -0400

    Few more validations need to be entered, deleted rough drafts

--- script.js
+++ script.js
@@ -3,22 +3,6 @@ let validate = document.querySelector('#submit-button')
 let inputFields = document.querySelectorAll('input')
 let formIsValid
 
-form.addEventListener('submit', function(event){
-    validateName()
-    valiadteCarYr()
-    validateCarMk()
-    validateCarMd()
-    validateDate()
-    validateDays()
-    validateCardNumber()
-    validateCVV()
-    validateExpiration()
-    event.preventDefault()
-    //only submit when these are valid. How to prove they are valid?
-
-    //must create a total amount owed when submit button is entered and valid. The cost is $5 per weekday, and $7 per weekend day. `.map` and `.reduce` will be very helpful in calculating the total cost. will use #total
-})
-
 function makeInvalid () {
     return 0
 } 
@@ -26,27 +10,6 @@ function makeInvalid () {
 function makeValid (){
     return 1
 } 
-
-validate.onclick = function (){
-    for (let inputField of inputFields) {
-        let inputValue = inputField.value
-        let parentEl = inputField.parentElement
-        let parent = inputField.parentNode
-        let text = document.createTextNode("This field is Required") //is there a way to make "This field" say which item is required?
-        let divRef = document.createElement("div")
-        if (inputValue.length <= 0) {
-            parentEl.classList.remove("input-valid")
-            parentEl.classList.add("input-invalid")
-            divRef.appendChild(text)
-            parent.appendChild(divRef) //cant get this to stop repeating
-            formIsValid = false
-        } else {
-            parentEl.classList.remove("input-invalid") //code breaks with multiple entries
-            parentEl.classList.add("input-valid")
-            parent.removeChild(parent.lastChild)
-        }    
-    }
-}
 
 function validateName() {
     let inputName = document.getElementById("name")
@@ -60,36 +23,50 @@ function validateName() {
 
 function valiadteCarYr() {
     let inputYear = document.querySelector('#car-year')
-    //must be a number
     if (Number.isInteger(inputYear.value) && inputYear.length === 4 && inputYear > 1900 && inputYear < 2020) {
         return makeValid(inputYear)
     }else {
-        return makeInvalid
+        return makeInvalid(inputYear)
     }
-    //must be after 1900 
-    //must not be in the future
-    //must be vaild
 }
 
 function validateCarMk() {
     let inputCarMk = document.querySelector('#car-make')
+    if (inputCarMk.value <= 0) {
+        return makeInvalid(inputCarMk)
+    } else {
+        return makeValid(inputCarMk)
+    }
 }
 
 function validateCarMd() {
     let inputCarMd = document.querySelector('#car-model')
+    if (inputCarMd.value <= 0) {
+        return makeInvalid(inputCarMd)
+    } else {
+        return makeValid(inputCarMd)
+    }
 }
-
 
 function validateDate() {
     let inputDate = document.querySelector('#start-date')
-    //must be a future date
-    //must be vaild
+    //must be a future date code does not prevent it from past date but looks right. Issue in validation?
+    let todayDate = Date.now()
+    if (inputDate.value < todayDate) {
+        return makeInvalid(inputDate) 
+    } else {
+        return makeValid(inputDate)
+    }
 }
 
 function validateDays() {
     let inputDays = document.querySelector('#days')
     //days must be a number
-    //must be between 1 and 30
+    if (Number.isInteger(inputDays.value) && inputDays >= 1 && inputDays <= 30) {
+        return makeValid(inputDays)
+    } else {
+        return makeInvalid(inputDays)
+    }
     //must be vaild
 }
 
@@ -108,10 +85,10 @@ function luhnCheck(val) {
     return (sum % 10) == 0;
 }
 
-function validateCardNumber(cardNumber) {
+function validateCardNumber(number) {
+    let cardNumber = document.querySelector('#credit-card')
     let regex = new RegExp("^[0-9]{16}$");
-    // let cardNumber = document.querySelector('#credit-card')
-    if (!regex.test(cardNumber))
+    if (!regex.test(number))
         return false;
 
     return luhnCheck(cardNumber);
@@ -131,3 +108,40 @@ function validateExpiration() {
     //date must not be in the past
     //must be vaild
 }
+
+validate.onclick = function (){
+    for (let inputField of inputFields) {
+        let inputValue = inputField.value
+        let parentEl = inputField.parentElement
+        let parent = inputField.parentNode
+        let text = document.createTextNode("This field is Required") //is there a way to make "This field" say which item is required?
+        let divRef = document.createElement("div")
+        if (inputValue.length <= 0) {
+            parentEl.classList.remove("input-valid")
+            parentEl.classList.add("input-invalid")
+            divRef.appendChild(text)
+            parent.appendChild(divRef) //cant get this to stop repeating
+            formIsValid = false
+        } else {
+            parentEl.classList.remove("input-invalid") //code breaks either at credit card or make inputs
+            parentEl.classList.add("input-valid")
+            parent.removeChild(parent.lastChild)
+        }    
+    }
+}
+
+form.addEventListener('submit', function(event){
+    validateName()
+    valiadteCarYr()
+    validateCarMk()
+    validateCarMd()
+    validateDate()
+    validateDays()
+    validateCardNumber()
+    validateCVV()
+    validateExpiration()
+    event.preventDefault()
+    //only submit when these are valid. How to prove they are valid?
+
+    //must create a total amount owed when submit button is entered and valid. The cost is $5 per weekday, and $7 per weekend day. `.map` and `.reduce` will be very helpful in calculating the total cost. will use #total
+})
